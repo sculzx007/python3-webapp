5722a42372c0fd429bd2c311ee2c6433d574efd1
sculzx007 <sculzx007@qq.com>
2018-04-30 21:38:25 +0800

    完善RequestHandler，增加add_route()和add_routes()函数

--- python3-webapp/www/coroweb.py
+++ python3-webapp/www/coroweb.py
@@ -141,5 +141,91 @@ class RequestHandler(object):
                     for k, v in parse.parse_qs(qs, True).items():
                         kw[k] = v[0]
 
+        if kw is None:
+            kw = dict(**request.match_info)
+        else:
+            if not self._has_var_kw_arg and self._named_kw_args:
+            # if fn does not have **kw and fn has KEYWORD_ONLY
+            # remove all unamed kw
+                copy = dict()
+            for name in self._named_kw_args :
+                if name in kw:
+                    copy[name] = kw[name]
+            kw = copy
+
+            # check named arg
+            for k, v in request.match_info.items():
+                if k in kw:
+                    logging.warning('Duplicate arg name in named arg and kw args: %s' % k)
+                kw[k] = v
+
+        if self._has_request_arg:
+            kw['request'] = request
+
+        # check required kw
+        if self._required_kw_args:
+            for name in self._required_kw_args:
+                if not name in kw:
+                    return web.HTTPBadRequest('Missing argument: %s' % name)
+        logging.info('call with args: %s' % str(kw))
+
+        try:
+            # self._func is input 'fn'
+            r = await self._func(**kw)
+            return r
+        except APIError as e:
+            return dict(error = e.error, data = e.data, message = e.message)
+
+def add_static(app):
+    path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static')
+    app.router.add_static('/static/', path)
+    logging.info('add static %s ==> %s' % ('/static/', path))
+
+# 用于注册一个URL函数
+def add_route(app, fn):
+    method = getattr(fn, '__method__', None)
+    path = getattr(fn, '__route__', None)
+    if path is None or method is None:
+        raise ValueError('@get or @post not defined in %s.' % str(fn))
+    if not asyncio.iscoroutinefunction(fn) and not inspect.isgeneratorfunction(fn):
+        fn = asyncio.coroutine(fn)
+
+    logging.info('add route %s %s ==> %s(%s)' % (method, path, fn.__name__, '.'.join(inspect.signature(fn).parameters.keys())))
+    app.router.add_route(method, path, RequestHandler(app, fn))
+
+    # please note that 'RequestHandler(app, fn)' here is callable.
+    # Thus it acts like a faction (in fact an instance).
+
+# 自动把Handler模块的所有符合条件的函数注册了
+def add_routes(app, module_name):
+    '''
+    1. module_name' is another module consisting url handlers.
+    Please note that 'module_name' might also be in the form of 'package.module'. In this case, we need to retrieve the name of module we want (module_name[n+1:]) and set 'fromlist' in __import__ function as the name of module.
+
+    2. __import__(name, globals=None, locals=None, fromlist=(), level=0)
+
+    3. when name = package.module:
+    if fromlist is not assigned, then we return package; else we will return package.fromlist
+    '''
+    n = module_name.rfind('.')
+    if n == (-1):
+        mod = __import__(module_name, globals(), locals())
+    else:
+        name = module_name[n+1:]
+        mod = getattr(__import__(module_name[:n], globals(), locals()))
+
+    for attr in dir(mod):
+        if attr.startwith('_'):
+            continue
+        fn = getattr(mod, attr)
+        if callable(fn):
+            method = getattr(fn, '__method__', None)
+            path = getattr(fn, '__route__', None)
+            if method and path:
+                add_route(app, fn)
+
+
+
+
 
 
