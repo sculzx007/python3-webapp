8828bec383f847819f781003db8ef69c2c687796
sculzx007 <sculzx007@qq.com>
2018-04-30 21:37:16 +0800

    增加jinja2模版和自注册支持

--- python3-webapp/www/app.py
+++ python3-webapp/www/app.py
@@ -10,10 +10,60 @@ import logging; logging.basicConfig(level=logging.INFO)
 import asyncio, os, json, time, aiomysql
 from datetime import datetime
 from aiohttp import web
+from jinja2 import Environment, FileSystemLoader
+import orm
+from coroweb import add_routes, add_static
+
+def init_jinja2(app, **kw):
+    logging.info('init jinja2...')
+
+    # 在Jinja2模板中，用{{ name }}表示一个需要替换的变量,用{% ... %}表示指令
+    options = dict(autoescape = kw.get('autoescape', True),
+                   block_start_string = kw.get('block_start_string', '{%'),
+                   block_end_string = kw.get('block_end_string', '%}'),
+                   variable_start_string = kw.get('variable_start_string', '{{'),
+                   variable_end_string = kw.get('variable_end_string', '}}'),
+                   auto_reload = kw.get('auto_reload', True)
+                   )
+    path = kw.get('path', None)
+    if path is None:
+        # __file__ means the module file itself
+        path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
+    logging.info('set jinja2 template path: %s' % path)
+    env = Environment(loader = FileSystemLoader(path), **options)
+    filters = kw.get('filters', None)
+    if filters is not None:
+        for name, f in filters.items():
+            env.filters[name] = f
+    app['__templating__'] = env
+
+# middleware #1: logger_factory: to log info of urls before handler.
+async def logger_factory(app, handler):
+    async def logger(request):
+        logging.info('Request: %s %s' % (request.method, request.path))
+        return (await handler(request))
+        # after logging continue other tasks by calling handler().
+        # handler here is an instance but callable (see in coroweb.py).
+    return logger
+
+# middleware #2: authentication
+async def data_factory(app, handler):
+    async def parse_data(request):
+        if request.method == 'POST':
+            if request.content_type.startswith('application/json'):
+                request.__data__ = await request.json()
+                logging.info('request json: %s' %  str(request.__data__))
+            elif request.content_type.startswith('application/x-www-form-urlencoded'):
+                request.__data__ = request.post()
+                logging.info('request form: %s' % str(request.__data__))
+        return (await handler(request))
+    return parse_data
+
+
+
 
 def index(request):
     return web.Respone(body = b'<h1>Awesome</h1>')
-
 
 async def init(loop):
     app = web.Application(loop= loop)
