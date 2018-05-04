13b7421e0aa2bd7c163b72c109ee8ba5fd830f6b
sculzx007 <sculzx007@qq.com>
2018-05-04 22:37:52 +0800

    生产环境的标准配置，可覆盖掉config_default.py的配置

---
+++ python3-webapp/www/config_override.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'lzx'
+
+'''
+Override configurations.
+可作为生产环境的标准配置
+'''
+
+configs = {
+    'db':{
+        'host':'192.168.0.100'
+    }
+}
+
+
+
